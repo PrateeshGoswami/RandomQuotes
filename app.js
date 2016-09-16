0201de4d143e9231064431004aca449772e33620
Prateesh Goswami <prateeshgoswami@gmail.com>
2016-09-16 10:39:53 -0700

    Add jquery to change background color

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-$(document).ready(function() {
-    // Only change code below this line.
-    $("#getQuotes").on("click", function(){
+ 
+ $(document).ready(function(){
 
+   // jQuery methods go here...
+var colors = ["#CCCCCC","#333333","#990099","tomato","red","blue","green","grey"];                
+  var rand = Math.floor(Math.random()*colors.length); 
+
+   $("#co").click(function() {
+    $("body").css("background-color",colors[rand]);
 });
-    // Only change code above this line.
-  });
+ 
+ });
