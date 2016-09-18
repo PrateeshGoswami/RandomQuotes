dc69bb1c919c030b4cf5cc482e7c45950350cc9d
Prateesh Goswami <prateeshgoswami@gmail.com>
2016-09-17 17:50:18 -0700

    Add url to fetch quotes

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
  
- $(document).ready(function(){
+$(document).ready(function(){
 
    // jQuery methods go here...
-var colors = ["#CCCCCC","#333333","#990099","tomato","red","blue","green","grey"];                
-  var rand = Math.floor(Math.random()*colors.length); 
 
-   $("#co").click(function() {
-    $("body").css("background-color",colors[rand]);
-});
+	   $("#co").on("click", function() {
+	   	var colors = ["#CCCCCC","#333333","#990099","tomato","red","blue","green","grey"];                
+	    var rand = colors[Math.floor(Math.random()*colors.length)]; 
+
+		    $("body").css("background-color",rand);
+		    $("#quote").text("This is a quote")
+		     $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
+              $(".quote").html(JSON.stringify(json));
+             });
+		      function logResults(json){
+ 				 console.log(json);
+				}
+
+					$.ajax({
+					  url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+					  dataType: "jsonp",
+					  jsonpCallback: "logResults"
+					});
+   		});
  
- });
+});
