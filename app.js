32702be8931500212b82474376eafe51caa18fa1
Prateesh Goswami <prateeshgoswami@gmail.com>
2016-09-21 18:30:56 -0700

    add ajax instead of getjson

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
- 
+
+function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } } 
+var currentQuote = '', currentAuthor = '';
 $(document).ready(function(){
+	$.ajaxSetup({
+		cache: false //needed in order to update quotes
+	});
 
    // jQuery methods go here...
 
@@ -8,19 +13,22 @@ $(document).ready(function(){
 	    var rand = colors[Math.floor(Math.random()*colors.length)]; 
 
 		    $("body").css("background-color",rand);
-		    $("#quote").text("This is a quote")
-		     $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(json) {
-              $(".quote").html(JSON.stringify(json));
-             });
-		      function logResults(json){
- 				 console.log(json);
-				}
-
-					$.ajax({
-					  url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
-					  dataType: "jsonp",
-					  jsonpCallback: "logResults"
-					});
+		    // $("#quote").text("This is a quote")
+		   $.ajax({
+		   	 headers: {
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+  type: 'GET',
+  dataType: 'json',
+  url: 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en',
+  success: function(response){
+  	var r = JSON.parse(response);
+  	currentQuote = r.quote;
+    $("#quote").text(currentQuote);
+  }
+});
+		
    		});
  
 });
